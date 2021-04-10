df4eec7af4effdf8506969ef03d45955fedc96a6
NoncreativeUsername <71220035+NoncreativeUsername@users.noreply.github.com>
2021-04-10 10:58:29 -0500

    Update 452_final.ino

--- 452_final.ino
+++ 452_final.ino
@@ -2,18 +2,19 @@
   -------------LCD---------------
   
   The circuit:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 6
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+    LCD RS pin to digital pin 7
+    LCD Enable pin to digital pin 6
+    LCD D4 pin to digital pin 5
+    LCD D5 pin to digital pin 4
+    LCD D6 pin to digital pin 3
+    LCD D7 pin to digital pin 2
+    LCD R/W pin to ground
+    LCD VSS pin to ground
+    LCD VDD pin to 5V
+    A to 10K resistor to 5V:
+    K to Ground
+    wiper to LCD VO pin (pin 3)
+
 
 
 -------------POT0-----------------
